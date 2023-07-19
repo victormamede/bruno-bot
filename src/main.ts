d6eb0c03c66dd7d490040fa8c92e1328102efb28
Bruno Santos <48936982+brunosanntos@users.noreply.github.com>
2023-07-19 10:15:10 -0300

    criado arquivo, alterado para aspas duplas

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Client, LocalAuth } from "whatsapp-web.js";
+import { nettoInsults } from "./utils/nettoInsults";
 
 const groupId = process.env.GROUP_ID;
 
@@ -9,6 +10,8 @@ const client = new Client({
   },
 });
 
+const learning: string[] = [...nettoInsults, "Remulo tambem é gay ✨"];
+
 client.on("qr", (qr) => {
   console.log(qr);
 });
@@ -18,29 +21,54 @@ client.on("ready", () => {
 });
 
 client.on("message", async (msg) => {
-  if (msg.from !== groupId) {
+  if (msg.from === groupId) {
+    learning.push(msg.body);
+
+    if (Math.random() < 0.2) {
+      const randomIndex = Math.floor(Math.random() * learning.length);
+      const random = learning[randomIndex];
+      msg.reply(random);
+    }
+
+    if (
+      msg.body === "!netto" ||
+      msg.body === "!neto" ||
+      msg.body.includes("neto")
+    ) {
+      const randomIndex = Math.floor(Math.random() * nettoInsults.length);
+      const xingamento = nettoInsults[randomIndex];
+      msg.reply(xingamento);
+      return;
+    }
+
+    if (msg.body.startsWith("!ofender")) {
+      if (msg.from !== groupId) {
+        return;
+      }
+      const mentions = await msg.getMentions();
+      if (mentions.length > 0) {
+        const resp = await fetch("https://xinga-me.appspot.com/api");
+        const body = (await resp.json()) as { xingamento: string };
+
+        msg.reply(`@${mentions[0].id.user}, ${body.xingamento}`, undefined, {
+          mentions: [mentions[0]],
+        });
+      } else {
+        msg.reply("Eu vou xingar você se não largar de ser burro");
+      }
+      return;
+    }
+
     return;
   }
 
-  if (msg.body == "!figurinha") {
+  if (msg.body === "!figurinha") {
     if (msg.hasMedia) {
       msg.reply("Aguenta aí chefe");
       const media = await msg.downloadMedia();
       msg.reply(media, undefined, { sendMediaAsSticker: true });
     } else {
       msg.reply("Mano, você é burro?");
-    }
-  } else if (msg.body.startsWith("!ofender")) {
-    const mentions = await msg.getMentions();
-    if (mentions.length > 0) {
-      const resp = await fetch("https://xinga-me.appspot.com/api");
-      const body = (await resp.json()) as { xingamento: string };
-
-      msg.reply(`@${mentions[0].id.user}, ${body.xingamento}`, undefined, {
-        mentions: [mentions[0]],
-      });
-    } else {
-      msg.reply("Eu vou xingar você se não largar de ser burro");
     }
   }
 });
---
+++ src/utils/nettoInsults.ts
@@ -0,0 +1,11 @@
+export const nettoInsults = [
+  "sim netto é boiola gosta de chupar pika e quicar num cacete grande e ficar rebolando quando vê homem sarado ✨",
+  "siiiiiim netinho muito gay pra mim ✨",
+  "sim netto gosta de dançar de salto alto e fazer carão no espelho 💅",
+  "nossa, netinho gay pra mim ✨",
+  "sim netto é apaixonado por unicórnios e coleciona todos os produtos relacionados 🦄",
+  "sim netto é usuario de Crocs 👟",
+  "sim netto tem elo WhatsApp fake no Valorant 😂",
+  "Netto é muito domador de piroca",
+  "netinho gay pra mim ✨",
+];
